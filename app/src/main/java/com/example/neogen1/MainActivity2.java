3e3d4c17f16030aef1c33ef12908f98bd3550ad8
jrevanthreddy <jrevanth11@gmail.com>
2024-02-02 15:50:29 +0530

    first commit

--- app/src/main/java/com/example/neogen1/MainActivity2.java
+++ app/src/main/java/com/example/neogen1/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.neogen1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,24 +14,29 @@ import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    TextView sign_up,forget_password;
+    TextView sign_up,forgot_password;
     EditText email,password;
     Button sign_in_btm;
+    ImageView facebook,google;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         sign_up=findViewById(R.id.sign_up);
-        forget_password=findViewById(R.id.forget_password);
+        forgot_password=findViewById(R.id.forget_password);
         email=findViewById(R.id.sign_in_email);
         password=findViewById(R.id.sign_in_password);
         sign_in_btm=findViewById(R.id.sing_in_btn);
+        facebook=findViewById(R.id.sign_in_facebook);
+        google=findViewById(R.id.sign_in_google);
 
 
         sign_up.setOnClickListener(v -> startActivity(new Intent(MainActivity2.this, Register_Page.class)));
         sign_in_btm.setOnClickListener(v -> attentLogin());
-
+        facebook.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/login/"))));
+        google.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://mail.google.com/mail/u/0/"))));
+        forgot_password.setOnClickListener(v -> startActivity(new Intent(MainActivity2.this,com.example.neogen1.forgot_password.class)));
     }
 
     private void attentLogin() {
